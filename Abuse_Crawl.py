97cf2336c4bd4b1f98fef907cb308f9f895e4020
beendoo <songbeen96@gmail.com>
2023-08-31 16:03:32 +0900

    ✨ Abuse_Crawl First

---
+++ Abuse_Crawl.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import sys, os
+from variableFile import AbuseAPI
+
+
+# Defining the api-endpoint
+url = 'https://api.abuseipdb.com/api/v2/check'
+
+querystring = {
+    'ipAddress': '118.25.6.39',
+    'maxAgeInDays': '90'
+}
+
+headers = {
+    'Accept': 'application/json',
+    'Key': AbuseAPI
+}
+
+response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+
+# Formatted output
+decodedResponse = json.loads(response.text)
+print(json.dumps(decodedResponse, sort_keys=True, indent=4))

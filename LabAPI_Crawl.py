262aad146c9244c52f50e764e5f59bc3c61656fb
beendoo <songbeen96@gmail.com>
2023-09-06 14:22:18 +0900

    ✅ 응답값 확인

--- LabAPI_Crawl.py
+++ LabAPI_Crawl.py
@@ -1,12 +1,16 @@
 import requests
 import json
+from tqdm import tqdm
 
 url = ('https://github.com/monitorapp-aicc/feed/blob/main/ip.txt')
 
-response = requests.request(method='GET', url=url)
+t = 0
+for t in tqdm(range(1), desc='Processing'):
+    response = requests.request(method='GET', url=url)
 
-# Formatted output
-decodedResponse = json.loads(response.text)
-#print(json.dumps(decodedResponse, sort_keys=True, indent=4))
+    # Formatted output
+    decodedResponse = json.loads(response.text)
+    #print(json.dumps(decodedResponse, sort_keys=True, indent=4))
 
-print(decodedResponse)
+    print(decodedResponse)
+

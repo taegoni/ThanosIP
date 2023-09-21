47bc83591e71306cb4e06e34989c72d1a1b2259a
pangyo_notebook <ssoulistic@gmail.com>
2023-09-21 18:52:16 +0900

    ✅ testing dbmodules

--- DB_writer.py
+++ DB_writer.py
@@ -1,30 +1,42 @@
-import mariadb
-import json
-import sys
+# import mariadb
+# import json
+# import sys
+# dbkeys=open("./etc/teniron.json",'r',encoding='utf-8')
+# db_meta=json.load(dbkeys)
 
-dbkeys=open("./etc/teniron.json",'r',encoding='utf-8')
-db_meta=json.load(dbkeys)
+# SERVER,PORT = db_meta["MariaDB"]["server"].split("/")
+# DATABASE= db_meta["MariaDB"]["database"]
+# USERNAME = 'team'
+# PASSWORD = db_meta["MariaDB"]["password"]
 
-SERVER,PORT = db_meta["MariaDB"]["server"].split("/")
-DATABASE= db_meta["MariaDB"]["database"]
-USERNAME = 'team'
-PASSWORD = db_meta["MariaDB"]["password"]
+# try:
+#     conn = mariadb.connect(user=USERNAME,password=PASSWORD,host=SERVER,port=PORT,database=DATABASE)
+# except mariadb.Error as e:
+#     print(f"Error connecting to MariaDB Platform: {e}")
+#     sys.exit(1)
 
-try:
-    conn = mariadb.connect(user=USERNAME,password=PASSWORD,host=SERVER,port=PORT,database=DATABASE)
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+# cur = conn.cursor()
 
-cur = conn.cursor()
-query="SELECT * FROM test_table"
+# query="SELECT * FROM test_table"
 
-cur.execute(query)
+# cur.execute(query)
 
-rows = cur.fetchall()
-print(rows,'\n')
+# rows = cur.fetchall()
+# print(rows,'\n')
 
-conn.commit()
+# conn.commit()
 
-cur.close()
-conn.close()
+# cur.close()
+# conn.close()
+
+import dbModule
+
+table_name="test_table"
+insert_data='"123.123.123.123",1'
+db_class = dbModule.Database()
+sql = f"INSERT INTO {table_name} VALUES({insert_data})"
+db_class.execute(sql)
+db_class.commit()
+
+
+
---
+++ dbModule.py
@@ -0,0 +1,35 @@
+import pymysql
+import json
+
+dbkeys=open("./etc/teniron.json",'r',encoding='utf-8')
+db_meta=json.load(dbkeys)
+
+SERVER,PORT = db_meta["MariaDB"]["server"].split("/")
+DATABASE= db_meta["MariaDB"]["database"]
+USERNAME = 'team'
+PASSWORD = db_meta["MariaDB"]["password"]
+
+class Database():
+    def __init__(self):
+        self.db = pymysql.connect(host=SERVER,
+                                  user=USERNAME,
+                                  password=PASSWORD,
+                                  db=DATABASE,
+                                  charset='utf8')
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+ 
+    def execute(self, query, args={}):
+        self.cursor.execute(query, args)  
+ 
+    def executeOne(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchone()
+        return row
+ 
+    def executeAll(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchall()
+        return row
+ 
+    def commit(self):
+        self.db.commit()

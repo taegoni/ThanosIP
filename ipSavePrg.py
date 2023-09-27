40f4d65c1edf85ededa4c61a41d27ce6040d9387
beendoo <songbeen96@gmail.com>
2023-09-27 14:13:50 +0900

    🐛

--- ipSavePrg.py
+++ ipSavePrg.py
@@ -42,15 +42,17 @@ while i < len(fileList):
             searchSql = f'SELECT ip FROM {table_name}'
             db_class.execute(searchSql)
             print("중간확인")
-            result = list(db_class.fetchall())
+            result = db_class.fetchall()
             print(result)
             # ip가 존재하지 않으면 DB에 입력 INSERT
-            if ip not in result:
-                sql = f"UPDATE {table_name} SET reputation_score = {reputation_score}, ip_from = {ip_from}, update_time = {update_time}"
-        # sql = "SELECT * from "+table_name
-            elif ip in result:
-                sql = f"INSERT INTO {table_name} VALUES({insert_data})" # SQL 쿼리문 => select,insert 모두 가능한데 지금은 입력만
+            for data in result:
+                if ip in data:
+                    sql = f"UPDATE {table_name} SET reputation_score = {reputation_score}, ip_from = {ip_from}, update_time = {update_time}"
+                    # sql = "SELECT * from "+table_name
             # ip가 이미 존재한다면 뒤의 3데이터만 덮어쓰기 UPDATE
+                else:
+                    sql = f"INSERT INTO {table_name} VALUES({insert_data})" # SQL 쿼리문 => select,insert 모두 가능한데 지금은 입력만
+            
             
             db_class.execute(sql) # db에 sql문 작성
             db_class.commit() #sql문 실행
